4db91469c3d0f4ef11694162b5d760bce8698cf1
KimH4nKyul <alskim93@kakao.com>
2023-12-03 02:59:26 +0900

    Test: 주소 지정하여 테스트

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,6 +2,5 @@ const ECommerceStore = artifacts.require('../ECommerceStore.sol')
 
 module.exports = function (deployer) {
   // deployer.link(ConvertLib, MetaCoin);
-  // accounts = web3.eth.getAccounts()
-  deployer.deploy(ECommerceStore, web3.eth.accounts[9])
+  deployer.deploy(ECommerceStore, '0x19c80879edef7E6C3206bda97F1A5Bb9B8b0930d')
 }
