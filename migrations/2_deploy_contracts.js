de4df5347cf5d1a77aa36d014fa3ce5cd292715c
KimH4nKyul <alskim93@kakao.com>
2023-12-03 02:55:48 +0900

    Test: ECommerStore에 중개 계정 패싱

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,6 +2,6 @@ const ECommerceStore = artifacts.require('../ECommerceStore.sol')
 
 module.exports = function (deployer) {
   // deployer.link(ConvertLib, MetaCoin);
-  accounts = web3.eth.getAccounts()
-  deployer.deploy(ECommerceStore, 'USE ONE OF THE 9 ACCOUNTS FROM GANACHE')
+  // accounts = web3.eth.getAccounts()
+  deployer.deploy(ECommerceStore, web3.eth.accounts[9])
 }
