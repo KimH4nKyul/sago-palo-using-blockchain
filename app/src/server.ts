f9fcf11e45c248f22446493cf4cc560ddf423588
KimH4nKyul <alskim93@kakao.com>
2023-12-08 06:33:54 +0900

    refactor: server runner

--- app/src/server.ts
+++ app/src/server.ts
@@ -2,20 +2,20 @@ import http from 'http'
 import { app } from './app'
 import { initMongoDb, mongoDbEventHandler } from './shared/startup'
 
-const server = http.createServer(app)
+const run = async () => {
+  const server = http.createServer(app)
 
-const PORT = process.env.NODE_PORT || 3000
-server.listen(PORT, async () => {
-  console.log(`Server is running on port ${PORT}`)
-
-  try {
-    if (!process.env.MONGODB_URL) {
-      throw new Error('MONGODB_URL Not found')
-    }
-
-    mongoDbEventHandler()
-    await initMongoDb(process.env.MONGODB_URL)
-  } catch {
-    process.exit(-1)
+  if (!process.env.MONGODB_URL) {
+    throw new Error('MONGODB_URL Not found')
   }
-})
+
+  mongoDbEventHandler()
+  await initMongoDb(process.env.MONGODB_URL)
+
+  const PORT = process.env.NODE_PORT || 3000
+  server.listen(PORT, async () => {
+    console.log(`Server is running on port ${PORT}`)
+  })
+}
+
+run()
