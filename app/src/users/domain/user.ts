ad84009230ae1bd4112783c372169bd9624e3ccf
KimH4nKyul <alskim93@kakao.com>
2023-12-09 15:48:26 +0900

    feature: user domain logics

--- app/src/users/domain/user.ts
+++ app/src/users/domain/user.ts
@@ -1,4 +1,18 @@
+enum LoginStatus {
+  LOGIN = 'LOGIN',
+  LOGOUT = 'LOGOUT',
+}
+
+enum UserRole {
+  AMDIN = 'ADMIN',
+  GENERAL = 'GENERAL',
+  COMPANY = 'COMPANY',
+}
+
 export class User {
+  private _isLogin: LoginStatus = LoginStatus.LOGOUT
+  private _userRole: UserRole = UserRole.GENERAL
+
   constructor(
     private readonly _id: string,
     private readonly _password: string,
@@ -11,6 +25,26 @@ export class User {
 
   static create(_id: string, _password: string): User {
     return new User(_id, _password)
+  }
+
+  login() {
+    this._isLogin = LoginStatus.LOGIN
+  }
+
+  logout() {
+    this._isLogin = LoginStatus.LOGOUT
+  }
+
+  set userRole(role: UserRole) {
+    this._userRole = role
+  }
+
+  get userRole(): UserRole {
+    return this._userRole
+  }
+
+  get isLogin(): LoginStatus {
+    return this._isLogin
   }
 
   get password(): string {
