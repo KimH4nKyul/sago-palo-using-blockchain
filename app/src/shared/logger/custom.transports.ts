ca14b30d3c2e4c2a54625cc72375d6f798cb9244
KimH4nKyul <alskim93@kakao.com>
2023-12-10 20:38:45 +0900

    refactor: transports's options now managed by a option object

--- app/src/shared/logger/custom.transports.ts
+++ app/src/shared/logger/custom.transports.ts
@@ -9,25 +9,26 @@ const infoFilter = format((info) => {
   return info.level === 'info' ? info : false
 })
 
-const httpFileOpts = {
-  level: 'http',
-  filename: 'log/access-%DATE%.http.log',
-  datePattern: 'YYYY-MM-DD',
-  maxSize: '50m',
-  maxFiles: '14d',
-  zippedArchive: true,
-  format: httpFilter(),
+const opts = {
+  httpFile: {
+    level: 'http',
+    filename: 'log/access-%DATE%.http.log',
+    datePattern: 'YYYY-MM-DD',
+    maxSize: '50m',
+    maxFiles: '14d',
+    zippedArchive: true,
+    format: httpFilter(),
+  },
+  infoFile: {
+    level: 'info',
+    filename: 'log/access-%DATE%.info.log',
+    datePattern: 'YYYY-MM-DD',
+    maxSize: '50m',
+    maxFiles: '14d',
+    zippedArchive: true,
+    format: infoFilter(),
+  },
 }
 
-const infoFileOpts = {
-  level: 'info',
-  filename: 'log/access-%DATE%.info.log',
-  datePattern: 'YYYY-MM-DD',
-  maxSize: '50m',
-  maxFiles: '14d',
-  zippedArchive: true,
-  format: infoFilter(),
-}
-
-export const httpFileTransports = new DailyRotateFile(httpFileOpts)
-export const infoFileTransports = new DailyRotateFile(infoFileOpts)
+export const httpFileTransports = new DailyRotateFile(opts.httpFile)
+export const infoFileTransports = new DailyRotateFile(opts.infoFile)
