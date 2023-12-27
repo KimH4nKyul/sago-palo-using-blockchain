5edf4cb81e05f36f65bf0e911aacfd54e7f0fdca
KimH4nKyul <alskim93@kakao.com>
2023-12-28 05:42:07 +0900

    :truck: 올바른 경로 설정

--- app/src/users/service/user.read.service.ts
+++ app/src/users/service/user.read.service.ts
@@ -1,6 +1,6 @@
 import { User } from '../../core/domain/users/user'
-import { UserReadUsecase } from '../../core/domain/users/port/user.read.usecase'
-import { UserRepository } from '../../core/domain/users/port/user.repository'
+import { UserReadUsecase } from '../../core/domain/users/interfaces/usecase/user.read.usecase'
+import { UserRepository } from '../../core/domain/users/interfaces/repository/user.repository'
 
 export class UserReadService implements UserReadUsecase {
   constructor(private readonly _userRepository: UserRepository) {}
